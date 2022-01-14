0eeef925ed44f7c556f42a496b8c8e78ba591d7e
Henrik Tudborg <het@keepfocus.dk>
2022-01-14 09:56:54 +0100

    The Exmbus.Key module was refactored to be a bit more ergonomic

--- lib/exmbus/key.ex
+++ lib/exmbus/key.ex
@@ -8,24 +8,43 @@ defmodule Exmbus.Key do
     %__MODULE__{keyfn: fun}
   end
 
-  def from_options(%{key: %__MODULE__{}=key}=opts, ctx) do
-    keys_for_ctx(key, opts, ctx)
-  end
-  def from_options(%{key: key_bytes}, _ctx) when is_binary(key_bytes) do
-    {:ok, [key_bytes]}
-  end
-  def from_options(%{key: list_of_key_bytes}, _ctx) when is_list(list_of_key_bytes) do
-    {:ok, list_of_key_bytes}
-  end
-  # no key given, return empty result
-  def from_options(%{}=_opts, _ctx) do
-    {:ok, []}
+  @doc """
+  Given a Key struct, get the keys, handing the key struct the options and current context.
+  Returns a list of keys
+  """
+  @spec get(%Exmbus.Key{}, options :: map(), ctx :: list()) :: {:ok, [binary()]} | {:error, reason :: any(), ctx :: list()}
+  def get(%__MODULE__{keyfn: keyfn}, opts, ctx) do
+    case keyfn.(opts, ctx) do
+      {:ok, keys} when is_list(keys) -> {:ok, keys}
+      {:error, reason} -> {:error, reason, ctx}
+      {:error, _e, error_ctx}=e when is_list(error_ctx) -> e
+    end
   end
 
-  defp keys_for_ctx(%__MODULE__{keyfn: fun}, opts, ctx) do
-    case fun.(opts, ctx) do
-      {:ok, keys} when is_list(keys) -> {:ok, keys}
+  @doc """
+  Short-hand for getting the key struct out of the option key :key
+  and calling key on that.
+
+  See get/3
+  """
+  def get(%{key: _}=opts, ctx) do
+    with key <- from_options!(opts) do
+      get(key, opts, ctx)
     end
+  end
+
+  def from_options!(%{key: %__MODULE__{}=key}) do
+    key
+  end
+  def from_options!(%{key: key_bytes}) when is_binary(key_bytes) do
+    %__MODULE__{keyfn: fn(_,_) -> {:ok, [key_bytes]} end}
+  end
+  def from_options!(%{key: list_of_key_bytes}) when is_list(list_of_key_bytes) and is_binary(hd(list_of_key_bytes)) do
+    %__MODULE__{keyfn: fn(_,_) -> {:ok, list_of_key_bytes} end}
+    {:ok, list_of_key_bytes}
+  end
+  def from_options!(%{}=opts) do
+    raise "the map key :key not found in options: #{inspect opts}"
   end
 
 end
