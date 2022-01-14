a36cd6e71d65c92be8b208791f88104f5f12aff3
Henrik Tudborg <het@keepfocus.dk>
2022-01-14 21:58:43 +0100

    Adding more tests from the OMS spec

--- test/oms_vol2_annex_n_test.exs
+++ test/oms_vol2_annex_n_test.exs
@@ -242,4 +242,181 @@ defmodule OMSVol2AnnexNTest do
              ] = ctx
     end
   end
+
+  describe "N.5 Heat Cost Allocator" do
+    # ACC-NR is not yet implemented
+    @tag :skip
+    test "N.5.2 wM-Bus Example with ACC-NR " do
+      frame = "194793444433221155378C20758B8877665593445508FF040000" |> Base.decode16!()
+      assert {:ok, ctx, <<>>} = Exmbus.parse(frame)
+    end
+
+    test "N.5.3 wM-Bus Example with partial encryption" do
+      frame =
+        "304493444433221155378C00757288776655934455080004100500DFE2A782146D1513581CD2F83F39040CFD1078563412"
+        |> Base.decode16!()
+
+      key = "000102030405060708090A0B0C0D0E0F" |> Base.decode16!()
+      assert {:ok, ctx, <<>>} = Exmbus.parse(frame, key: key)
+
+      assert [
+               %Exmbus.Apl.FullFrame{
+                 manufacturer_bytes: "",
+                 records: [
+                   %Exmbus.Apl.DataRecord{
+                     data: 1234,
+                     header: %Exmbus.Apl.DataRecord.Header{
+                       coding: :type_a,
+                       dib: %Exmbus.Apl.DataRecord.DataInformationBlock{
+                         data_type: :bcd,
+                         device: 0,
+                         function_field: :instantaneous,
+                         size: 24,
+                         storage: 0,
+                         tariff: 0
+                       },
+                       dib_bytes: "\v",
+                       vib: %Exmbus.Apl.DataRecord.ValueInformationBlock{
+                         coding: nil,
+                         description: :units_for_hca,
+                         extensions: [],
+                         multiplier: nil,
+                         table: :main,
+                         unit: nil
+                       },
+                       vib_bytes: "n"
+                     }
+                   },
+                   %Exmbus.Apl.DataRecord{
+                     data: ~D[2007-04-30],
+                     header: %Exmbus.Apl.DataRecord.Header{
+                       coding: :type_g,
+                       dib: %Exmbus.Apl.DataRecord.DataInformationBlock{
+                         data_type: :int_or_bin,
+                         device: 0,
+                         function_field: :instantaneous,
+                         size: 16,
+                         storage: 1,
+                         tariff: 0
+                       },
+                       dib_bytes: "B",
+                       vib: %Exmbus.Apl.DataRecord.ValueInformationBlock{
+                         coding: :type_g,
+                         description: :date,
+                         extensions: [],
+                         multiplier: nil,
+                         table: :main,
+                         unit: nil
+                       },
+                       vib_bytes: "l"
+                     }
+                   },
+                   %Exmbus.Apl.DataRecord{
+                     data: 23456,
+                     header: %Exmbus.Apl.DataRecord.Header{
+                       coding: :type_a,
+                       dib: %Exmbus.Apl.DataRecord.DataInformationBlock{
+                         data_type: :bcd,
+                         device: 0,
+                         function_field: :instantaneous,
+                         size: 24,
+                         storage: 1,
+                         tariff: 0
+                       },
+                       dib_bytes: "K",
+                       vib: %Exmbus.Apl.DataRecord.ValueInformationBlock{
+                         coding: nil,
+                         description: :units_for_hca,
+                         extensions: [],
+                         multiplier: nil,
+                         table: :main,
+                         unit: nil
+                       },
+                       vib_bytes: "n"
+                     }
+                   },
+                   %Exmbus.Apl.DataRecord{
+                     data: 12_345_678,
+                     header: %Exmbus.Apl.DataRecord.Header{
+                       coding: :type_a,
+                       dib: %Exmbus.Apl.DataRecord.DataInformationBlock{
+                         data_type: :bcd,
+                         device: 0,
+                         function_field: :instantaneous,
+                         size: 32,
+                         storage: 0,
+                         tariff: 0
+                       },
+                       dib_bytes: "\f",
+                       vib: %Exmbus.Apl.DataRecord.ValueInformationBlock{
+                         coding: :type_a,
+                         description: :customer_location,
+                         extensions: [],
+                         multiplier: nil,
+                         table: :fd,
+                         unit: nil
+                       },
+                       vib_bytes: <<253, 16>>
+                     }
+                   }
+                 ]
+               },
+               %Exmbus.Tpl{
+                 frame_type: :full_frame,
+                 header: %Exmbus.Tpl.Long{
+                   access_no: 0,
+                   configuration_field: %Exmbus.Tpl.ConfigurationField{
+                     accessibility: false,
+                     bidirectional: false,
+                     blocks: 1,
+                     content_of_message: 0,
+                     hop_count: 0,
+                     mode: 5,
+                     repeater_access: 0,
+                     syncrony: false
+                   },
+                   device: :heat_cost_allocator,
+                   identification_no: 55_667_788,
+                   manufacturer: "QDS",
+                   status: %Exmbus.Tpl.Status{
+                     application_status: :no_error,
+                     low_power: true,
+                     manufacturer_status: 0,
+                     permanent_error: false,
+                     temporary_error: false
+                   },
+                   version: 85
+                 }
+               },
+               %Exmbus.Ell{
+                 access_no: 117,
+                 communication_control: %Exmbus.Ell.CommunicationControl{
+                   accessibility: false,
+                   bidirectional: false,
+                   hop_count: false,
+                   priority: false,
+                   repeated_access: false,
+                   response_delay: :slow_delay,
+                   synchronized: false
+                 },
+                 session_number: nil
+               },
+               %Exmbus.Dll.Wmbus{
+                 control: :snd_nr,
+                 device: :radio_converter_meter_side,
+                 identification_no: 11_223_344,
+                 manufacturer: "QDS",
+                 version: 85
+               }
+             ] = ctx
+    end
+
+    @tag :skip
+    test "N.5.4 M-Bus Example with partial encryption" do
+      # TODO: fill in this frame from the example (page 46)
+      frame = "" |> Base.decode16!()
+      key = "000102030405060708090A0B0C0D0E0F" |> Base.decode16!()
+      assert {:ok, ctx, <<>>} = Exmbus.parse(frame, key: key)
+    end
+  end
 end
