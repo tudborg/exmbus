d885827bafab89515fa1bd3f5cffc761af8c97a1
Henrik Tudborg <henrik@tudb.org>
2024-11-12 11:24:36 +0100

    Add default_handlers/0, append_handlers/2 and added more docs.
    Also merge warnings by appending them (like errors)

--- lib/exmbus/parser/context.ex
+++ lib/exmbus/parser/context.ex
@@ -38,13 +38,13 @@ defmodule Exmbus.Parser.Context do
   ]
 
   defstruct [
+    # remaining binary data
+    bin: nil,
     # configuration:
     opts: %{},
     # handlers to apply, in order:
-    handlers: @default_handlers,
+    handlers: nil,
     __current_handler__: nil,
-    # remaining binary data
-    bin: nil,
     # lower layers:
     dll: nil,
     ell: nil,
@@ -64,10 +64,31 @@ defmodule Exmbus.Parser.Context do
     warnings: []
   ]
 
-  def new(attrs \\ []) do
-    merge(%__MODULE__{}, attrs)
+  @doc """
+  Return the list of default handlers.
+  """
+  def default_handlers() do
+    @default_handlers
   end
 
+  @doc """
+  Create a new context  with default handlers, and merge the given attributes into the context.
+  """
+  def new(attrs \\ []) do
+    %__MODULE__{}
+    |> append_handlers(default_handlers())
+    |> merge(attrs)
+  end
+
+  @doc """
+  Merge the given attributes into the context.
+
+  The attributes are merged in the following way:
+  - `:errors` - appended to the existing list of errors
+  - `:warnings` - appended to the existing list of warnings
+  - `:opts` - merged with the existing opts
+  - any other key - overwritten
+  """
   def merge(%__MODULE__{} = ctx, attrs \\ []) do
     Enum.reduce(attrs, ctx, &merge_key/2)
   end
@@ -76,6 +97,10 @@ defmodule Exmbus.Parser.Context do
   defp merge_key({:errors, errors}, ctx),
     do: Map.update(ctx, :errors, [], &(&1 ++ errors))
 
+  # warnings are appended:
+  defp merge_key({:warnings, warnings}, ctx),
+    do: Map.update(ctx, :warnings, [], &(&1 ++ warnings))
+
   # optionsare Map.merge/2'ed:
   defp merge_key({:opts, opts}, ctx),
     do: %{ctx | opts: Map.merge(ctx.opts, Enum.into(opts || [], %{}))}
@@ -83,6 +108,13 @@ defmodule Exmbus.Parser.Context do
   # any other key is overwritten:
   defp merge_key({key, value}, ctx),
     do: %{ctx | key => value}
+
+  @doc """
+  Append additional handlers to the context.
+  """
+  def append_handlers(%__MODULE__{} = ctx, handlers) do
+    %__MODULE__{ctx | handlers: (ctx.handlers || []) ++ handlers}
+  end
 
   @doc """
   Apply the context to the next handler in the list.
